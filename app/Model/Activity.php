196ddf722b47f875caf60b00b2da7d4793419403
Мирослав Здравковић <miroslav@fimanager.com>
2017-11-17 08:43:45 +0100

    Relationships in Club and Activity model

--- app/Model/Activity.php
+++ app/Model/Activity.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
-    //
+    public function club ()
+    {
+    	return $this->belongsTo(Product::class);
+    }
 }
--- app/Model/Club.php
+++ app/Model/Club.php
@@ -2,9 +2,15 @@
 
 namespace App\Model;
 
+use App\Model\Activity;
 use Illuminate\Database\Eloquent\Model;
 
 class Club extends Model
 {
     //
+
+    public function activities ()
+    {
+    	return $this->hasMany(Activity::class);
+    }
 }
